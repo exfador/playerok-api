b691ab7f307fde6e826ae782e2834ef3c62eea95
exfador <71018041+exfador@users.noreply.github.com>
2026-04-03 14:56:03 +0300

    Delete pok directory

--- pok/client.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-from .conn import Conn, active_conn
-
-
-def get_account() -> Conn:
-    conn = active_conn()
-    if conn is None:
-        raise RuntimeError('Активное соединение Playerok не инициализировано')
-    return conn
