eb76ffd36f78fa5250f4f62efd81f51ca6957412
exfador <71018041+exfador@users.noreply.github.com>
2026-04-06 00:55:59 +0300

    Delete pok directory

--- pok/client.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-
-def get_account():
-    from bot.core import active_engine
-    eng = active_engine()
-    if eng is None:
-        raise RuntimeError('Движок не запущен')
-    return eng.account
