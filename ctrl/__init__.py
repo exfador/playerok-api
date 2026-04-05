80cff2312e425f359c832d2e42d12c4d9ed3a6cd
exfador <71018041+exfador@users.noreply.github.com>
2026-04-06 00:55:32 +0300

    Delete ctrl directory

--- ctrl/__init__.py
+++
@@ -1,6 +0,0 @@
-from aiogram import Router
-from .priority import router as priority_router
-from .actions import router as actions_router
-
-router = Router()
-router.include_routers(priority_router, actions_router)
--- ctrl/ui/__init__.py
+++
@@ -1,2 +0,0 @@
-from .main import *
-from .settings import *
